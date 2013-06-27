5f30de5c39bb8a1717c926cc58e77da2cc71335f
Michal Papis <mpapis@gmail.com>
2013-06-27 19:25:47 +0200

    updat rvm-capistrano version, stop using --deployment for bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,10 @@
+set :rvm_require_role, :app
+require "rvm/capistrano"
+require "bundler/capistrano"
+require "capistrano-unicorn"
+require "capistrano-file_db"
+load 'deploy/assets'
+
 set :application, "ad"
 
 set :deploy_to, "/home/#{application}/app"
@@ -9,6 +16,8 @@ set :keep_releases, 5
 
 set :rvm_autolibs_flag, "read-only" # more info: rvm help autolibs
 set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
+set :bundle_dir, ''
+set :bundle_flags, '--system --quiet'
 set :bundle_without,  [:development]
 set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 
@@ -22,9 +31,3 @@ before 'deploy',         'rvm:install_ruby'
 # Or create gemset
 #before 'deploy',         'rvm:create_gemset'
 after  'deploy',         'deploy:cleanup'
-
-require "rvm/capistrano"
-require "bundler/capistrano"
-require "capistrano-unicorn"
-require "capistrano-file_db"
-load 'deploy/assets'
