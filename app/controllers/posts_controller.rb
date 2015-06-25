489fc95d0f84857daa4e1977f68e4dff813d7569
Damien Bachet <dbachet@gmail.com>
2015-06-25 21:43:33 +0200

    refactor authorization part
    to squash to previous commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update]
+  before_action :authorize_user!, only: [:edit, :update]
 
   # GET /posts
   # GET /posts.json
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
+    find_post
 
     respond_to do |format|
       format.html # show.html.erb
@@ -36,14 +37,13 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
-    @post = Post.find(params[:id])
+    find_post
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
-    @post.user = current_user
+    @post = Post.new(post_params_for_create)
 
     respond_to do |format|
       if @post.save
@@ -59,13 +59,10 @@ class PostsController < ApplicationController
   # PUT /posts/1
   # PUT /posts/1.json
   def update
-    @post = Post.find(params[:id])
+    find_post
 
     respond_to do |format|
-      if !user_is_post_author?
-        format.html { redirect_to @post, error: 'You are not allowed to update that resource!' }
-        format.json { head :no_content }
-      elsif @post.update_attributes(post_params)
+      if @post.update_attributes(post_params)
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
       else
@@ -89,11 +86,29 @@ class PostsController < ApplicationController
 
   private
 
+  def find_post
+    @post ||= Post.find(params[:id])
+  end
+
+  def authorize_user!
+    find_post
+    if !user_is_post_author?
+      respond_to do |format|
+        format.html { redirect_to action: :index; flash[:alert] = 'You are not allowed to update that resource!' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   def user_is_post_author?
     current_user.id == @post.user_id
   end
 
   def post_params
-    params.require(:post).permit(:body, :title, :user_id)
+    params.require(:post).permit(:body, :title)
+  end
+
+  def post_params_for_create
+    post_params.merge(user: current_user)
   end
 end
